322cd566e0431ea163218648160586c6d5a99240
/ZEX <ghader2g2@gmail.com>
2023-10-30 19:46:50 +0400

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -1,87 +1,80 @@
 import requests
-import socket
-import sys
-import os
 from rich.console import Console
-from rich.prompt import Prompt
-from rich.progress import track, BarColumn, TextColumn
-from rich.panel import Panel
+from rich.table import Table
+from rich.progress import track
+import socket
 
 console = Console()
 
 PROXY_URLS = {
-    "1": ("socks5", "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks5&timeout=10000&country=all&ssl=all&anonymity=all"),
-    "2": ("socks4", "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks4&timeout=10000&country=all&ssl=all&anonymity=all")
+    "socks5": [
+        "https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks5.txt",
+        "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks5&timeout=10000&country=all&ssl=all&anonymity=all"
+    ],
+    "socks4": [
+        "https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks4.txt",
+        "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks4&timeout=10000&country=all&ssl=all&anonymity=all"
+    ]
 }
 
-def fetch_proxies(proxy_type, url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        proxies = response.text.strip().split("\n")
-        console.print(Panel(f"[green]Fetched {len(proxies)} proxies of type {proxy_type}.[/green]"))
-        return proxies
-    except requests.RequestException as e:
-        console.print(Panel(f"[red]Error fetching proxies: {e}[/red]"))
-        sys.exit(1)
+def fetch_proxies(proxy_type):
+    proxies = []
+    for url in PROXY_URLS[proxy_type]:
+        try:
+            response = requests.get(url, timeout=10)
+            response.raise_for_status()
+            proxies.extend(response.text.strip().split("\n"))
+        except requests.RequestException:
+            console.print(f"Failed to fetch proxies from [red]{url}[/red]")
+    return proxies
 
 def test_proxy(proxy, proxy_type, timeout):
     ip, port = proxy.split(":")
-    s = socket.socket()
-    s.settimeout(timeout)
     try:
-        s.connect((ip, int(port)))
-        s.close()
-        return True
+        if proxy_type == "socks5":
+            s = socket.create_connection((ip, int(port)), timeout=timeout)
+            s.close()
+            return True
+        elif proxy_type == "socks4":
+            s = socket.create_connection((ip, int(port)), timeout=timeout)
+            s.close()
+            return True
     except:
         return False
 
 def main():
-    console.print(Panel("[bold red]ZEX Proxy List[/bold red]"))
-    try:
-        choice = Prompt.ask("Choose proxy type:\n[1] socks5\n[2] socks4\n\n", choices=["1", "2"])
-        proxy_type, url = PROXY_URLS[choice]
-        proxies = fetch_proxies(proxy_type, url)
+    console.print("[bold blue]ZEX Proxy List[/bold blue]", justify="center")
+    console.print("Choose proxy type:\n[1] socks5\n[2] socks4\n\n", justify="left")
+    choice = console.input("Enter your choice: ")
 
-        num_proxies = Prompt.ask(f"How many {proxy_type} proxies would you like to test? (Leave empty for all)", default=str(len(proxies)))
-        proxies = proxies[:int(num_proxies)]
+    if choice == "1":
+        proxy_type = "socks5"
+    elif choice == "2":
+        proxy_type = "socks4"
+    else:
+        console.print("[red]Invalid choice![/red]")
+        return
 
-        test_choice = Prompt.ask("Would you like to test the proxies? (y/n)", choices=["y", "n"], default="y")
+    proxies = fetch_proxies(proxy_type)
+    console.print(f"Fetched [green]{len(proxies)} {proxy_type}[/green] proxies.")
 
-        if test_choice == "y":
-            timeout = Prompt.ask("Enter timeout for testing (default is 1 second):", default="1")
-            timeout = float(timeout)
-            working_proxies = []
+    save_without_test = console.input("Do you want to save proxies without testing? (y/n): ")
+    if save_without_test.lower() == 'y':
+        with open(f"{proxy_type}_zex.txt", "w") as f:
+            f.write("\n".join(proxies))
+        console.print(f"Saved [green]{len(proxies)} {proxy_type}[/green] proxies to [green]{proxy_type}_zex.txt[/green].")
+        return
 
-            for proxy in track(proxies, description="Testing proxies"):
-                if test_proxy(proxy, proxy_type, timeout):
-                    working_proxies.append(proxy)
-                    console.print(f"[green]Testing {proxy} - Status: Working[/green]")
-                else:
-                    console.print(f"[red]Testing {proxy} - Status: Failed[/red]")
+    timeout = float(console.input("Enter timeout for testing (default is 1): ") or 1)
+    working_proxies = []
+    for proxy in track(proxies, description=f"Testing {proxy_type} proxies..."):
+        if test_proxy(proxy, proxy_type, timeout):
+            working_proxies.append(proxy)
 
-            filename = f"{proxy_type}_zex.txt"
-            if os.path.exists(filename):
-                os.remove(filename)
+    with open(f"{proxy_type}_zex.txt", "w") as f:
+        f.write("\n".join(working_proxies))
 
-            with open(filename, "w") as f:
-                for proxy in working_proxies:
-                    f.write(proxy + "\n")
-
-            console.print(Panel(f"[green]Saved {len(working_proxies)} working proxies to {filename}[/green]"))
-        else:
-            filename = f"{proxy_type}_zex.txt"
-            if os.path.exists(filename):
-                os.remove(filename)
-
-            with open(filename, "w") as f:
-                for proxy in proxies:
-                    f.write(proxy + "\n")
-            console.print(Panel(f"[green]Saved {len(proxies)} proxies to {filename} without testing.[/green]"))
-    except ValueError:
-        console.print(Panel("[red]Invalid input provided![/red]"))
-    except Exception as e:
-        console.print(Panel(f"[red]An unexpected error occurred: {e}[/red]"))
+    console.print(f"Saved [green]{len(working_proxies)} {proxy_type}[/green] proxies to [green]{proxy_type}_zex.txt[/green].")
 
 if __name__ == "__main__":
     main()
